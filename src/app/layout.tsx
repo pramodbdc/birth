52d3145fa90751924288cf0f6a89e9565f9c5ed7
Pramod KUMAR <pramodcomputer542@gmail.com>
2026-01-07 08:46:38 +0000

    remove all thing related firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
       </head>
       <body className="font-body antialiased">
-        {children}
+        <main className="container mx-auto px-4 py-8">
+            {children}
+        </main>
         <Toaster />
       </body>
     </html>
